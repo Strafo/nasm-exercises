747821ab73c890ced4f7c24d21ccc19a999df985
strafoxultrapowa <aspem2015@gmail.com>
2019-09-14 20:55:39 +0200

    aggiunti commenti

--- echo.asm
+++ echo.asm
@@ -13,11 +13,11 @@ main:
 	sub 	rsp,0d100	;reserving space for vars
 	mov 	rdi,format	
 	lea	rsi,[rsp]
-	xor 	rax,rax
+	xor 	rax,rax		;https://stackoverflow.com/questions/6212665/why-is-eax-zeroed-before-a-call-to-printf
 	call	scanf		
 	mov	rdi,format	;load pointer to format string
 	lea	rsi,[rsp]	;todo
-	xor 	rax,rax
+	xor 	rax,rax		;same as scanf 
 	call	printf
 	xor 	rax,rax		
 	leave 
